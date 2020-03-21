bfdfd30bfd20541d117dc5a991c08b986455b43c
Jérémy Berteloot <jeremy@macbook-air-de-jeremy.home>
2020-03-21 17:00:23 +0100

    Added react-router, home & new plant page, started plants list

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+import HomeScreen from './Screens/HomeScreen';
+import NewPlantScreen from './Screens/NewPlantScreen';
+
+
+const Stack = createStackNavigator();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'baskerville-bold': require('./assets/fonts/LibreBaskerville-Bold.ttf'),
+    'baskerville-italic': require('./assets/fonts/LibreBaskerville-Italic.ttf'),
+    'baskerville-regular': require('./assets/fonts/LibreBaskerville-Regular.ttf')
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    )
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Mes plantes' }} />
+        <Stack.Screen
+          name="NewPlant"
+          component={NewPlantScreen}
+          options={{ title: 'Create a new plant' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
